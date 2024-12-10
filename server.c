64c0e22ccda63fc234f50ae0c994289673387d5d
Michal Sokala <1.michal3002@gmail.com>
2024-12-10 14:32:51 +0100

    changes

--- server.c
+++ server.c
@@ -1,3 +1,15 @@
-//
-// Created by Michał on 07.12.2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+
+int main() {
+    const char *list[2];
+    list[0] = "H";
+    list[1] = "T";
+
+    for (int i = 0; i < 20; i++) {
+        int rand_index = rand() % 2;
+        printf("rand element: %s\n",list[rand_index]);
+    }
+}
