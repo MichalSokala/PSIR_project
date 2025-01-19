0533592db4d21383c0e32a8aec118fd778416a30
Michał Krzyżanowski <michal.13777@gmail.com>
2025-01-19 22:25:23 +0100

    working version of program

--- node.cpp
+++ node.cpp
@@ -17,7 +17,7 @@
 #define SEQUENCE_PAYLOAD_TYPE 3
 
 #define CLIENT_ID 1
-#define MAX_BUFFER_LEN 10
+#define MAX_BUFFER_LEN 4
 #define SEQUENCE_LENGTH 6
 #define RETR_TIME 2000 // IN milliseconds
 
@@ -30,12 +30,11 @@ ZsutIPAddress address_ip = ZsutIPAddress(192, 168, 253, 10); //adres ip serwera
 uint8_t packetBuffer[MAX_BUFFER_LEN];
 unsigned int localPort;
 int r, packetSize;
-uint16_t t;
 bool winnerInfoAnswered=true, registerAnswered=true;
-uint16_t sequence, tossCount;
+uint16_t tossCount;
+uint8_t sequence;
 
-uint32_t lastServRegisterMessTime, lastServWinnerInfMessTime;// moze sie przydac jesli chcemy miec tez w wezle co ma sie stac jak
-// serwer przestanie dzialac
+uint32_t lastServRegisterMessTime, lastServWinnerInfoMessTime;
 
 ZsutEthernetUDP Udp;
 byte mac[] = MAC;
@@ -46,7 +45,7 @@ typedef struct {
     uint8_t retr_flag: 1;
     uint8_t ack_flag: 1;
     uint8_t p_type: 2;
-    uint32_t payload: 18;
+    uint32_t payload: 14;
 } Header;
 
 Header packetHeader;
@@ -57,13 +56,14 @@ void packMessageBuffer(Header *header, uint8_t *buffer) {
     buffer[1] = (header->p_type << 6);
 
     if(header->p_type == 1){
-        buffer[1] = (header->payload << 5);
+        buffer[1] = buffer[1] | (header->payload << 5);
     }else if(header->p_type == 2){
-        buffer[1] = buffer[1] | (header->payload >> 16);
-        buffer[2] = (header->payload >> 8) & 0xFF;
-        buffer[3] = header->payload & 0xFF;
+        buffer[1] |= ((header->payload >> 8) & 0x3F);
+        buffer[2] = header->payload & 0xFF;
     }else if(header->p_type == 3){
         buffer[1] = buffer[1] | header->payload;
+    }else{
+        header->payload = 0;
     }
 }
 
@@ -77,22 +77,23 @@ void unpackMessageBuffer(Header *header, uint8_t *buffer) {
     if(header->p_type == 1){
         header->payload = (buffer[1] >> 5) & 0x01;
     }else if(header->p_type == 2){
-        header->payload = buffer[1] & 0x3F;
-        header->payload = header->payload | buffer[2];
-        header->payload = header->payload | buffer[3];
+        header->payload = (buffer[1] & 0x3F) << 8;
+        header->payload |= buffer[2];
     }else if(header->p_type == 3){
         header->payload = buffer[1] & 0x3F;
+    }else{
+        header->payload = 0;
     }
-    Serial.print("HEADER p_Type");
-    Serial.println(header->p_type);
-    Serial.print("HEADER payload");
-    Serial.println(header->payload);
 }
 
 void sendMessage(Header header, uint8_t *buffer) {
     packMessageBuffer(&header, buffer);
+    if(header.msg_type == 3){
+        Serial.print("AFTER PACK: ");
+        Serial.println(buffer[3]);
+    }
     Udp.beginPacket(address_ip, PORT);
-    Udp.write(buffer, sizeof(buffer));
+    Udp.write(buffer, MAX_BUFFER_LEN);
     Udp.endPacket();
 }
 
@@ -111,7 +112,7 @@ void sendRegisterMsg(uint8_t *buffer) {
     lastServRegisterMessTime = ZsutMillis();
 }
 
-void sendWinnerInfo(uint8_t *buffer) {
+void sendWinnerInfo(uint8_t *buffer, uint16_t tossC, uint8_t seq) {
     Header header;
     header = {
             .msg_type = PATTERN_MATCH_MSG_TYPE,
@@ -119,10 +120,10 @@ void sendWinnerInfo(uint8_t *buffer) {
             .retr_flag = 0,
             .ack_flag = 0,
             .p_type = RESULT_PAYLOAD_TYPE,
-            .payload = ((sequence & 0x3F) << 12) | (tossCount & 0xFFF)
+            .payload = ((seq & 0x3F) << 8) | (tossC & 0xFF)
     };
     sendMessage(header, buffer);
-    lastServWinnerInfMessTime = ZsutMillis();
+    lastServWinnerInfoMessTime = ZsutMillis();
 }
 
 void setup() {
@@ -152,63 +153,56 @@ void loop() {
             Serial.println(F("WARNING: UDP buffer exceeded, got: "));
             Serial.println(packetSize);
         }
-        uint8_t packetBuffer[MAX_BUFFER_LEN];
-        r = Udp.read(packetBuffer, sizeof(packetBuffer));
-        char message[100];
-        snprintf(message, sizeof(message), "buffer0: %d buffer1: %d ", packetBuffer[1], packetBuffer[2]);
-        Serial.println(message);
-        Header packetHeader;
+        r = Udp.read(packetBuffer, MAX_BUFFER_LEN);
         unpackMessageBuffer(&packetHeader, packetBuffer);
 
         if(packetHeader.msg_type == TOSS_RESULT_MSG_TYPE) {
-            Serial.println(F("DEBUG: recv single toss result start: "));
+//            Serial.println(F("DEBUG: recv single toss result start: "));
             tossCount++;
             packetHeader.ack_flag = 1;
-            Serial.print("Toss result: ");
-            Serial.println(packetHeader.payload);
             sendMessage(packetHeader, packetBuffer);
+//            Serial.println(F("SENT ACK FOR TOSS_RESULT MESSAGE"));
+//            Serial.println(packetHeader.msg_type);
             sequence = (sequence << 1) | (packetHeader.payload & 0x1);
-            Serial.print("Sequence to match: ");
-            Serial.println(sequence);
             if (tossCount >= SEQUENCE_LENGTH && (sequence & 0x3F) == SEQUENCE) {
-                Serial.println(F("DEBUG: FOUND matching pattern"));
+                Serial.print(F("FOUND matching pattern"));
                 Serial.println(sequence);
-                sendWinnerInfo(packetBuffer);
+                Serial.print("Toss count: ");
+                Serial.println(tossCount);
+                sendWinnerInfo(packetBuffer, tossCount, sequence);
                 winnerInfoAnswered = false;
             }
-            Serial.println(F("DEBUG: recv single toss result end"));
+//            Serial.println(F("DEBUG: recv single toss result end"));
         }else if(packetHeader.msg_type == GAME_END_MSG_TYPE) {
-            Serial.println(F("This round has ended. Next one will start shortly."));
+//            Serial.println(F("This round has ended. Next one will start shortly."));
             sequence = 0;
             tossCount = 0;
             packetHeader.ack_flag = 1;
-            Serial.println(F("Started sending GAME_END ack"));
+//            Serial.println(F("Started sending GAME_END ack"));
             sendMessage(packetHeader, packetBuffer);
-            Serial.println(F("Ended sending GAME_END ack"));
+//            Serial.println(F("Ended sending GAME_END ack"));
+        }else if(packetHeader.msg_type == REGISTER_MSG_TYPE && packetHeader.ack_flag == 1){
+            registerAnswered = true;
+        }else if(packetHeader.msg_type == PATTERN_MATCH_MSG_TYPE && packetHeader.ack_flag == 1){
+            winnerInfoAnswered = true;
         }
-
-//        }else if(packetHeader.msg_type == REGISTER_MSG_TYPE && packetHeader.ack_flag == 1){
-//            registerAnswered = true;
-//        }else if(packetHeader.msg_type == PATTERN_MATCH_MSG_TYPE && packetHeader.ack_flag == 1){
-//            winnerInfoAnswered = true;
-//        }
     }
 
-//    if(ZsutMillis() - lastServRegisterMessTime > RETR_TIME && !registerAnswered){
-//        packetHeader = {
-//            .msg_type = REGISTER_MSG_TYPE,
-//            .id = CLIENT_ID,
-//            .retr_flag = 1,
-//            .ack_flag = 0,
-//            .p_type = SEQUENCE_PAYLOAD_TYPE,
-//            .payload = SEQUENCE
-//        };
-//        sendMessage(packetHeader, packetBuffer);
-//        registerAnswered = false;
-//        lastServRegisterMessTime = ZsutMillis();
-//    }
-//
-//    if(ZsutMillis() - lastServWinnerInfMessTime > RETR_TIME && !winnerInfoAnswered){
+    if(ZsutMillis() - lastServRegisterMessTime > RETR_TIME && !registerAnswered){
+        packetHeader = {
+            .msg_type = REGISTER_MSG_TYPE,
+            .id = CLIENT_ID,
+            .retr_flag = 1,
+            .ack_flag = 0,
+            .p_type = SEQUENCE_PAYLOAD_TYPE,
+            .payload = SEQUENCE
+        };
+        sendMessage(packetHeader, packetBuffer);
+        registerAnswered = false;
+        lastServRegisterMessTime = ZsutMillis();
+    }
+
+//    if(ZsutMillis() - lastServWinnerInfoMessTime > RETR_TIME && !winnerInfoAnswered){
 //        packetHeader = {
 //            .msg_type = PATTERN_MATCH_MSG_TYPE,
 //            .id = CLIENT_ID,
@@ -219,6 +213,6 @@ void loop() {
 //        };
 //        sendMessage(packetHeader, packetBuffer);
 //        winnerInfoAnswered = false;
-//        lastServWinnerInfMessTime = ZsutMillis();
+//        lastServWinnerInfoMessTime = ZsutMillis();
 //    }
 }
--- server.c
+++ server.c
@@ -17,13 +17,13 @@
 #define TOSSES_LIMIT  12 // IN BITS
 #define SEQUENCE_LENGTH 6 // IN BITS
 #define PORT "8080"
-#define NUMBER_OF_GAMES 5
+#define NUMBER_OF_GAMES 200
 #define MAX_PLAYERS 8
-#define SYMBOL_GEN_INTERVAL 1000 // IN MILLISECONDS
-#define BUFFER_SIZE 3 // IN BYTES
+#define SYMBOL_GEN_INTERVAL 100 // IN MILLISECONDS
+#define BUFFER_SIZE 5 // IN BYTES
 #define PLAYER_GATHERING_TIME 15000 // IN MILLISECONDS
 #define RETR_TIME 2000 // IN MILLISECONDS
-#define TIMEOUT 5000 // IN MILLISECONDS
+#define TIMEOUT 10000 // IN MILLISECONDS
 
 typedef struct{
     uint8_t msg_type :3;
@@ -31,7 +31,7 @@ typedef struct{
     uint8_t retr_flag :1;
     uint8_t ack_flag :1;
     uint8_t p_type :2;
-    uint32_t payload :18;
+    uint32_t payload :14;
 } Header;
 
 typedef struct{
@@ -78,29 +78,30 @@ void pack_header(Header *header, uint8_t *buffer){
     buffer[1] = (header->p_type << 6);
 
     if(header->p_type == 1){
-        buffer[1] = (header->payload << 5);
+        buffer[1] = buffer[1] | (header->payload << 5);
     }else if(header->p_type == 2){
-        buffer[1] = buffer[1] | (header->payload >> 16);
-        buffer[2] = (header->payload >> 8) & 0xFF;
-        buffer[3] = header->payload & 0xFF;
+        buffer[1] = (header->payload >> 8) & 0x3F;
+        buffer[2] = header->payload;
     }else if(header->p_type == 3){
         buffer[1] = buffer[1] | header->payload;
     }
 }
 
 void unpack_header(uint8_t *buffer, Header *header){
-    header->msg_type = (buffer[0] >> 5) & 0x07;
-    header->id = (buffer[0] >> 2) & 0x07;
-    header->retr_flag = (buffer[0] >> 1) & 0x01;
-    header->ack_flag = buffer[0] & 0x01;
+    header->msg_type = (buffer[0] >> 5) & 0x7;
+    header->id = (buffer[0] >> 2) & 0x7;
+    header->retr_flag = (buffer[0] >> 1) & 0x1;
+    header->ack_flag = buffer[0] & 0x1;
     header->p_type = (buffer[1] >> 6) & 0x03;
 
     if(header->p_type == 1){
-        header->payload = (buffer[1] >> 5) & 0x01;
+        header->payload = (buffer[1] >> 5) & 0x1;
     }else if(header->p_type == 2){
-        header->payload = buffer[1] & 0x3F;
-        header->payload = header->payload | buffer[2];
-        header->payload = header->payload | buffer[3];
+        header->payload = (buffer[1] & 0x3F) << 8;
+        printf("PAYLOAD: %d\n", header->payload);
+        header->payload |= buffer[2];
+        printf("PAYLOAD: %d\n", header->payload);
+        printf("buffer0 %d buffer1 %d buffer2 %d\n", buffer[0], buffer[1], buffer[2]);
     }else if(header->p_type == 3){
         header->payload = buffer[1] & 0x3F;
     }
@@ -115,18 +116,16 @@ int send_toss_result_to_all(int sock, Player *player_list, int player_count,  ui
         .retr_flag = 0,
         .ack_flag = 0,
         .p_type = 1,
-        .payload = result & 0x01
+        .payload = result & 0x1
     };
 
     for(i = 0; i < player_count; i++){
         header.id = player_list[i].id;
         receiver = player_list[i].addr;
-
         pack_header(&header, buffer);
-        printf("buffer0 %d buffer1 %d\n", buffer[0], buffer[1]);
         player_list[i].result_ack = false;
 
-        res = sendto(sock, buffer, sizeof(buffer), 0,(struct sockaddr *) &receiver, sizeof(receiver));
+        res = sendto(sock, buffer, 3, 0,(struct sockaddr *) &receiver, sizeof(receiver));
 
         if (res < 0) {
             printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
@@ -148,10 +147,7 @@ int add_player(uint8_t id, Player *player_list, int *player_count, struct sockad
     return 0;
 }
 
-bool are_all_acks_received(Player* player_list, uint8_t player_count, AckType ack_type, uint8_t toss_count) {
-    if(toss_count == 0){
-        return true;
-    }
+bool are_all_acks_received(Player* player_list, uint8_t player_count, AckType ack_type) {
     for (uint8_t i = 0; i < player_count; i++) {
         if ((ack_type == RESULT_ACK && !player_list[i].result_ack) ||
             (ack_type == GAME_OVER_ACK && !player_list[i].game_over_ack)) {
@@ -200,10 +196,15 @@ void acknowledge_message(uint8_t player_id, Player *player_list, int player_coun
     }
 }
 
-//void save_winner(uint16_t sequence, uint16_t toss_count, Result *game_results, uint8_t game_number){
-//    Result result = {.sequence=sequence, .toss_count=toss_count};
-//    game_results[game_number] = result;
-//}
+bool is_their_sequence(uint8_t id, uint8_t sequence, Player *player_list, uint8_t player_count){
+    for(uint8_t i = 0; i < player_count; i++){
+        if(player_list[i].id == id && player_list[i].sequence == sequence){
+//            printf("Sequence %d is equal?%: %d\n", sequence, player_list[i].sequence == sequence);
+            return true;
+        }
+    }
+    return false;
+}
 
 int send_game_over_to_all(int sock, Player *player_list, uint8_t player_count){
     uint8_t buffer[BUFFER_SIZE], id;
@@ -266,22 +267,25 @@ void print_sequence_as_ht(uint16_t sequence){
     printf("%s", value);
 }
 
-void print_probability(Player *player_list, uint8_t player_count, Result *game_results, uint8_t game_number){
-    uint8_t counter;
-    for(uint8_t i = 0; i < player_count; i++){
+void print_probability(Player *player_list, uint8_t player_count, Result *game_results, uint16_t game_number){
+    uint16_t counter, number_of_tosses;
+    for(uint16_t i = 0; i < player_count; i++){
         counter = 0;
+        number_of_tosses = 0;
         for(uint8_t j = 0; j < game_number; j++){
-            printf("%d - %d\n",player_list[i].sequence, game_results[j].sequence);
             if(player_list[i].sequence == game_results[j].sequence){
                 counter++;
+                number_of_tosses += game_results[j].toss_count;
             }
         }
         printf("Probability for ");
         print_sequence_as_ht(player_list[i].sequence);
-        if(!counter){
-            printf(": %f%\n", 100*(float)counter/game_number);
+        if(counter){
+            printf(": %.2f %", 100*(float)counter/game_number);
+            printf(" average tosses per win: %.2f\n", (float)number_of_tosses/counter);
         }else{
-            printf(": 0.0%\n");
+            printf(": 0.00 %");
+            printf(" average tosses per win: 0.00\n");
         }
     }
 }
@@ -295,9 +299,11 @@ bool player_in_list(Player *player_list, uint8_t player_count, uint8_t player_id
     return false;
 }
 
-int make_one_game(int sock, Player *player_list, int player_count, Result game_results[], uint8_t game_number) {
+int make_one_game(int sock, Player *player_list, int player_count, Result game_results[], uint16_t game_number) {
     uint8_t result;
-    uint16_t toss_count = 0, winning_sequence;
+    uint16_t toss_count = 0;
+    uint8_t local_sequence = 0, winning_sequence;
+
     struct sockaddr_in c;
     int select_res, recv_res, c_len = sizeof(c);
     fd_set readfs;
@@ -325,74 +331,79 @@ int make_one_game(int sock, Player *player_list, int player_count, Result game_r
                 printf("ERROR: %s (%s:%d)\n", strerror(errno), __FILE__, __LINE__);
                 return -1;
             }
-//            printf("RECV: %d\n", buffer);
+
             unpack_header(buffer, &header);
 
             if(header.p_type == 2){
-                winning_sequence = header.payload >> 12;
-                toss_count = header.payload & 0xFFF;
+                winning_sequence = (header.payload >> 8) & 0x3F;
+                toss_count = header.payload & 0xFF;
+                printf("WINNING SEQUENCE %d \n", winning_sequence);
             }
             if(header.msg_type == TOSS_RESULT_MSG_TYPE && header.ack_flag){
                 acknowledge_message(header.id, player_list, player_count, RESULT_ACK);
-                printf("ACKNOWLEDGED RESPONSE for TOSS_RESULT message from ID %d\n", header.id);
+//                printf("ACKNOWLEDGED RESPONSE for TOSS_RESULT message from ID %d\n", header.id);
             }else if(header.msg_type == PATTERN_MATCH_MSG_TYPE){
-                printf("FOUND WINNER %d\n", header.id);
-                if(!found_winner){
-//                    save_winner(winning_sequence, toss_count, game_results, game_number);
+                printf("FOUND WINNER ID = %d\n", header.id);
+                if(!found_winner && is_their_sequence(header.id, winning_sequence, player_list, player_count)){
                     Result result_of_game = {.sequence=winning_sequence, .toss_count=toss_count};
-                    game_results[game_number] = result_of_game;
-                    printf("GAME RESULT: %d\n", game_results[game_number].sequence);
+                    game_results[game_number-1] = result_of_game;
+//                    printf("GAME RESULT: %d - %d\n", game_results[game_number-1].sequence);
                     found_winner = true;
                 }else{
-                    printf("Winner has been already found\n");
+                    printf("Win is not valid\n");
+                    printf("Whole payload: %d\n", header.payload);
                 }
             }else if(header.msg_type == GAME_END_MSG_TYPE && header.ack_flag){
                 acknowledge_message(header.id, player_list, player_count, GAME_OVER_ACK);
-                printf("ACKNOWLEDGED RESPONSE for GAME_END message from ID %d\n", header.id);
+//                printf("ACKNOWLEDGED RESPONSE for GAME_END message from ID %d\n", header.id);
             }else{
                 printf("Wrong type of message: %d\n", header.msg_type == 3);
             }
         }
 
-        current_time = get_time_ms();
-        all_result_acks_received = are_all_acks_received(player_list, player_count, RESULT_ACK, toss_count);
-        all_game_over_acks_received = are_all_acks_received(player_list, player_count, GAME_OVER_ACK, toss_count);
 
-        if(current_time - last_result_ack_check >= RETR_TIME && !all_result_acks_received){
-            send_result_retransmissions(sock, player_list, player_count, result);
+        if(current_time - last_result_ack_check >= RETR_TIME){
+            all_result_acks_received = are_all_acks_received(player_list, player_count, RESULT_ACK);
+            if(!all_result_acks_received){
+                send_result_retransmissions(sock, player_list, player_count, result);
+            }
             last_result_ack_check = current_time;
         }
 
-        if(current_time - last_game_over_ack_check >= RETR_TIME && !all_game_over_acks_received){
-            send_game_over_retransmissions(sock, player_list, player_count);
-            last_game_over_ack_check = current_time;
+        if(current_time - last_game_over_ack_check >= RETR_TIME){
+            all_game_over_acks_received = are_all_acks_received(player_list, player_count, GAME_OVER_ACK);
+            if(!all_game_over_acks_received){
+                send_game_over_retransmissions(sock, player_list, player_count);
+                last_game_over_ack_check = current_time;
+            }
         }
 
-//        if((current_time - last_time_tossed >= TIMEOUT && !all_result_acks_received) ||
-//            (current_time - game_over_first_sent >= TIMEOUT && !all_game_over_acks_received)){
-//            printf("Lost connection with one of players. Ending game...\n");
-//            return -1;
-//        }
-
+        current_time = get_time_ms();
 	    if(current_time - last_time_tossed >= SYMBOL_GEN_INTERVAL && (all_result_acks_received || first_time)){
 	        first_time = false;
             result = toss_coin();
-            printf("Outcome of toss: %c\n", result ? 'H' : 'T' );
+//            printf("Outcome of toss: %c\n", result ? 'H' : 'T' );
+            local_sequence = (local_sequence << 1) | result;
             send_toss_result_to_all(sock, player_list, player_count, result);
             last_time_tossed = current_time;
             last_result_ack_check = current_time;
         }
 
-        if(found_winner){
+        if(found_winner && all_result_acks_received){
 	            send_game_over_to_all(sock, player_list, player_count);
 	            found_winner = false;
-	            game_over_first_sent = get_time_ms();
+	            last_game_over_ack_check = get_time_ms();
 	    }
-	    if(all_game_over_acks_received && toss_count > 0){
-	        printf("INSIDE GAME ENDING IF\n");
+
+	    if(all_game_over_acks_received && toss_count > SEQUENCE_LENGTH){
 	        return 0;
 	    }
 
+
+        if(current_time - last_time_tossed >= TIMEOUT){
+            printf("Lost connection with one of players. Ending game...\n");
+            return -1;
+        }
 
     };
 }
@@ -465,7 +476,7 @@ int main() {
 	int player_count = 0;
 	Player player_list[MAX_PLAYERS];
     Result results[NUMBER_OF_GAMES];
-    uint8_t res, pos;
+    short res, pos;
 
 	h.ai_family = PF_INET;
 	h.ai_socktype = SOCK_DGRAM;
@@ -498,15 +509,16 @@ int main() {
 		exit(-1);
     }
 
-	for (int i = 0; i < NUMBER_OF_GAMES; i++) {
+	for (uint16_t i = 1; i <= NUMBER_OF_GAMES; i++) {
 	    res = make_one_game(s, player_list, player_count, results, i);
 		if(res < 0){
 		    break;
 		}else{
-		    printf("Game no. %d ended. Current probability for each used sequence:\n", i);
+		    printf("Game no. %d ended. Current statistics for each used sequence:\n", i);
+		    printf("Current sequence length: %d\n", SEQUENCE_LENGTH);
+		    printf("Current number of players: %d\n", player_count);
 		    print_probability(player_list, player_count, results, i);
 		}
-
 	}
 
 	freeaddrinfo(r);
